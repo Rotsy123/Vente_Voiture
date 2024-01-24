6c2e8f3114285fe68ae8725890dfc0a45ed25f47
ahava <minoraherinirina72@gmail.com>
2024-01-24 08:39:41 +0100

    stati

---
+++ src/main/java/tech/chillo/sa/model/StatistiqueUtilisateur.java
@@ -0,0 +1,45 @@
+package tech.chillo.sa.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import tech.chillo.sa.entites.Carburant;
+import tech.chillo.sa.entites.Transmission;
+
+public class StatistiqueUtilisateur {
+    private Personne personne;
+    private int nombre_annonce;
+    private int nombre_annonce_vendu;
+
+    public StatistiqueUtilisateur(Personne personne, int nombre_annonce, int nombre_annonce_vendu) {
+        this.setPersonne(personne);
+        this.setNombre_annonce(nombre_annonce);
+        this.setNombre_annonce_vendu(nombre_annonce_vendu);
+    }
+
+    public void setPersonne(Personne personne){
+        this.personne = personne;
+    }
+
+    public void setNombre_annonce(int nombre_annonce){
+        this.nombre_annonce = nombre_annonce;
+    }
+
+    public void setNombre_annonce_vendu(int nombre_annonce_vendu){
+        this.nombre_annonce_vendu = nombre_annonce_vendu;
+    }
+
+    public Personne getPersonne(){
+        return this.personne;
+    }
+
+    public int getNombre_annonce(){
+        return this.nombre_annonce;
+    }
+
+    public int getNombre_annonce_vendu(){
+        return this.nombre_annonce_vendu;
+    }
+
+
+
+}
