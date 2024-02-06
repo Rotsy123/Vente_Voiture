57472e41644c8e5535f48895b007f4cf8fbe3d91
Rotsy123 <rakotondrainyrotsy@gmail.com>
2024-02-06 23:23:02 +0300

    favoris

--- src/main/java/tech/chillo/sa/controller/AnnonceFavorisController.java
+++ src/main/java/tech/chillo/sa/controller/AnnonceFavorisController.java
@@ -1,13 +1,14 @@
 package tech.chillo.sa.controller;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import tech.chillo.sa.entites.AnnonceFavoris;
+import tech.chillo.sa.security.token.JwtUtils;
 import tech.chillo.sa.service.AnnonceFavorisService;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
@@ -17,18 +18,43 @@ import java.util.List;
 public class AnnonceFavorisController {
     private AnnonceFavorisService annonceFavorisService;
 
-    public AnnonceFavorisController(AnnonceFavorisService afs){
+    public AnnonceFavorisController(AnnonceFavorisService afs) {
         this.annonceFavorisService = afs;
     }
 
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping("/creerfavoris")
-    public void CreerFavoris(@RequestParam("idpersonne") int idpersonne, @RequestParam("idannonce") int idannonce){
-        this.annonceFavorisService.createAnnonceFavoris(idannonce, idpersonne);
+    @PreAuthorize("hasRole('USER')")
+    public void CreerFavoris(@RequestParam("idannonce") int idannonce,
+            @RequestHeader(name = "Authorization") String authorizationHeader) {
+        System.out.println("Token ---------------------------------------------- >>> : " + authorizationHeader);
+        JwtUtils jwt = new JwtUtils();
+        String token = authorizationHeader.substring(7);
+        System.out.println("Token : " + token);
+        try {
+            int id = jwt.getId(token);
+            System.out.println(id);
+            this.annonceFavorisService.createAnnonceFavoris(idannonce, id);
+
+        } catch (Exception e) {
+
+        }
     }
 
     @GetMapping(value = "list/{idpersonne}", produces = APPLICATION_JSON_VALUE)
-    public List<AnnonceFavoris> favorisListe(@PathVariable int idpersonne){
-        return this.annonceFavorisService.GetFavorisByPersonne(idpersonne);
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Object> favorisListe(
+            @RequestHeader(name = "Authorization") String authorizationHeader) {
+        JwtUtils jwt = new JwtUtils();
+        String token = authorizationHeader.substring(7);
+        System.out.println("Token : " + token);
+        try {
+            int id = jwt.getId(token);
+            System.out.println(id);
+            return new ResponseEntity<>(this.annonceFavorisService.GetFavorisByPersonne(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+
+        }
     }
 }
